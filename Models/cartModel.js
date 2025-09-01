3e41dd1b5ecded8a996b2d9293a3e5325d9f90fe
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2025-09-01 15:12:05 +0530

    second commit

--- Models/cartModel.js
+++ Models/cartModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
+const cartModel = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
